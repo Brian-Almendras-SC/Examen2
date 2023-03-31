116f3377acd3409096780fdefa0d859ea3d0d111
Brian <brian.almendras@ucb.edu.bo>
2023-03-30 20:46:08 -0400

    added recycler_view

--- app/src/main/java/com/ucb/examen2/MainActivity.kt
+++ app/src/main/java/com/ucb/examen2/MainActivity.kt
@@ -2,10 +2,23 @@ package com.ucb.examen2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var recyclerView: RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val list= arrayListOf<Book>(
+            Book("1","1","Primer libro","hola")
+        )
+        val linearLayoutManager= LinearLayoutManager(this)
+        linearLayoutManager.orientation= LinearLayoutManager.VERTICAL
+
+        recyclerView=findViewById(R.id.recycler_view)
+        recyclerView.layoutManager=linearLayoutManager
+        recyclerView.adapter=BookListAdapter(list,this)
+
     }
 }
